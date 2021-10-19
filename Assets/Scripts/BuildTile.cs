91703f7b1819e0ebe58a2403b3451eed06e775ae
Paweł Koch <sodment@gmail.com>
2021-10-19 22:03:50 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/BuildTile.cs
+++ Assets/Scripts/BuildTile.cs
@@ -4,8 +4,8 @@ using UnityEngine.Tilemaps;
 public class BuildTile : MonoBehaviour
 {
     private Grid grid;
-    [SerializeField] private Tilemap HighlightTilemap = null;
-    [SerializeField] private Tile hoverTile = null;
+    [SerializeField] private Tilemap highlightTilemap = null;
+    [SerializeField] private Tile highlightTile = null;
     public Vector3Int SelectedPosition;
     private Vector3Int previousMousePos = new Vector3Int();
     // Start is called before the first frame update
@@ -19,8 +19,8 @@ public class BuildTile : MonoBehaviour
         Vector3Int mousePos = GetMousePosition();
         if (!mousePos.Equals(previousMousePos))
         {
-            HighlightTilemap.SetTile(previousMousePos, null);
-            HighlightTower(mousePos, hoverTile);
+            highlightTilemap.SetTile(previousMousePos, null);
+            highlightTilemap.SetTile(mousePos, highlightTile);
             previousMousePos = mousePos;
         }
 
@@ -29,12 +29,6 @@ public class BuildTile : MonoBehaviour
             SelectedPosition = mousePos;
             Debug.Log("Selected Pos:" + SelectedPosition.ToString());
         }
-    }
-
-
-    void HighlightTower(Vector3Int tilePos, Tile highlightTile)
-    {
-        HighlightTilemap.SetTile(tilePos, highlightTile);
     }
 
     Vector3Int GetMousePosition()
