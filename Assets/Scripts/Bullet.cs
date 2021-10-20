9beb886b8fc38e1e10ec1f170d1eaf3dbf7756d5
cichy30002 <cichy30002@gmail.com>
2021-10-20 15:59:01 +0200

    inverced shooting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,23 +7,24 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float speed = 1f;
     [SerializeField] private float hitRange = 0.5f;
     private IEnumerator coroutine;
-    private IEnumerator Attack(Enemy enemy, float damage)
+    private IEnumerator Attack(Enemy enemy, Tower tower, float damage)
     {
-        while(Vector2.Distance(transform.position, enemy.transform.position) > hitRange)
+        enemy.AddDamage(damage);
+        while (Vector2.Distance(transform.position, tower.transform.position) > hitRange)
         {
-            Vector2 movingDirection = new Vector2(enemy.transform.position.x - transform.position.x, enemy.transform.position.y - transform.position.y).normalized;
+            Vector2 movingDirection = new Vector2(tower.transform.position.x - transform.position.x, tower.transform.position.y - transform.position.y).normalized;
             Vector2 newPosition = movingDirection * speed * Time.deltaTime ;
             transform.position += new Vector3(newPosition.x, newPosition.y, 0f);
             yield return null;
         }
         
-        enemy.AddDamage(damage);
+        
         Destroy(gameObject);
         yield return null;
     }
-    public void StartAttack(Enemy enemy, float damage)
+    public void StartAttack(Enemy enemy,Tower tower, float damage)
     {
-        coroutine = Attack(enemy, damage);
+        coroutine = Attack(enemy, tower, damage);
         StartCoroutine(coroutine);
     }
 }
