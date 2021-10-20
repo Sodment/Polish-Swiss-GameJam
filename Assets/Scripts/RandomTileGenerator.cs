001f3d95d99ea232414a77c1bf8ab35fa134311b
Kitkathere <helena-maslowska@wp.pl>
2021-10-20 18:13:30 +0200

    8lo ssie

--- Assets/Scripts/RandomTileGenerator.cs
+++ Assets/Scripts/RandomTileGenerator.cs
@@ -8,13 +8,21 @@ public class RandomTileGenerator : MonoBehaviour
     private Grid grid;
     [SerializeField] Tilemap tilemap;
     public List<Tile> grass_pieces;
-    // Start is called before the first frame update
+
     void Start()
     {
         grid = gameObject.GetComponent<Grid>();
+<<<<<<< Updated upstream
         for(int i=-20; i <= 20;i++)
         {
             for(int j=-15; j <= 15; j++)
+=======
+
+        for(int i=-12; i<12;i++)
+        {
+            for(int j=-7; j<7; j++)
+
+>>>>>>> Stashed changes
             {
                 Vector3Int pos = new Vector3Int(i,j,0);
                 int r = Random.Range(0, grass_pieces.Count);
@@ -23,7 +31,6 @@ public class RandomTileGenerator : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
