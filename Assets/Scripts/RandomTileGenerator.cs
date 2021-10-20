6e7326c95ff45ba27b39c1277473da265c325590
Paweł Koch <sodment@gmail.com>
2021-10-20 17:43:54 +0200

    Essa

--- Assets/Scripts/RandomTileGenerator.cs
+++ Assets/Scripts/RandomTileGenerator.cs
@@ -12,15 +12,9 @@ public class RandomTileGenerator : MonoBehaviour
     void Start()
     {
         grid = gameObject.GetComponent<Grid>();
-<<<<<<< Updated upstream
         for(int i=-20; i <= 20;i++)
         {
             for(int j=-15; j <= 15; j++)
-=======
-        for(int i=-12; i<12;i++)
-        {
-            for(int j=-7; j<7; j++)
->>>>>>> Stashed changes
             {
                 Vector3Int pos = new Vector3Int(i,j,0);
                 int r = Random.Range(0, grass_pieces.Count);
