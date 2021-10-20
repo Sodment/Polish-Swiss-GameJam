6458d5bf54dd75fbe6bcea26d1e24893e5f60fbf
Paweł Koch <sodment@gmail.com>
2021-10-20 16:07:02 +0200

    Add BuildTowers.cs

--- Assets/Scripts/BuildTowers.cs
+++ Assets/Scripts/BuildTowers.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 
 public class BuildTowers : MonoBehaviour
 {
-    public List<Vector3Int> takenTiles;
-    private SelectedTile selectedTile;
-
-
+    public List<Vector3Int> takenTiles = new List<Vector3Int>();
+    [SerializeField] private SelectedTile selectedTile;
 
     public void BuildTower(GameObject tower)
     {
         Vector3Int selected = selectedTile.GetSelectedTile();
+        Debug.Log(selected.ToString());
         if(takenTiles.Contains(selected))
         {
             Debug.Log("Its TAKEN");
