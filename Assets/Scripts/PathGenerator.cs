cf9d0e36844e8a678094055ce0a62e4033f8958d
Kitkathere <helena-maslowska@wp.pl>
2021-10-20 20:01:00 +0200

    Add turtle

---
+++ Assets/Scripts/PathGenerator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+    Vector3Int pos = new Vector3Int(-10,0,0);
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
